591567addde1ef1a7e795e9a68082877262e466c
armoney7 <112188406+armoney7@users.noreply.github.com>
2023-04-27 01:21:39 +0700

    Профилирование

--- Seminar_4/Task_1.py
+++ Seminar_4/Task_1.py
@@ -1,15 +1,11 @@
 import random
 
-n = int(input("Введите количество элементов первого множества: "))
-m = int(input("Введите количество элементов второго множества: "))
+n, m = int(input("Введите количество элементов первого множества: ")), int(input("Введите количество элементов второго множества: "))
 
-set1 = {random.randint(1, 100) for i in range(n)}
+set1, set2 = {random.randint(1, 100) for i in range(n)}, {random.randint(1, 100) for i in range(m)}
 
-set2 = {random.randint(1, 100) for i in range(m)}
-
-print("Первое множество:", set1)
-print("Второе множество:", set2)
+print(f"Первое множество: {set1}\nВторое множество: {set2}")
 
 intersection = sorted(set1 & set2)
 print("Числа, встречающиеся в обоих наборах:")
-print(", ".join(str(x) for x in intersection))
+print(", ".join(str(x) for x in intersection) + ".")
